f506d20365d94693e6e8fcfd497ce0b0165b2fbb
bryanor948 <l23121066@morelia.tecnm.mx8>
2026-07-02 13:57:51 -0700

    Git 3 v3.

--- hellogit3.py
+++ hellogit3.py
@@ -1,6 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("Hello Git 3! v2.")
 =======
 print("Git 3 v login.")
 
 >>>>>>> fcb858f (Git 3 login)
+=======
+print("Hello Git 3! v3.")
+>>>>>>> 1ac0e41 (Git 3 v3.)
